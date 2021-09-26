e9e4476d8aa6dc3d65200c5fb2be897fa2859bc9
Alisson Amaral <aalissonamaral98@gmail.com>
2021-09-26 15:47:02 -0300

    ajuste das variaveis de ambiente

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,32 @@
+const parse = require('pg-connection-string').parse;
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL", false) ? {
-          require: true,
-          rejectUnauthorized: false,
-        } : false,
+  if(env('NODE_EN') == 'production') {
+  const config = parse(process.env.DATABASE_URL);
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        options: {
+          ssl: true,
+        },
       },
-      options: {},
     },
-  },
+
+  }
+}
+
+
 });
